eef6d42bec50f4057d9cfac8323237d582c57644
oa8oht <leahliu779@gmail.com>
2019-06-22 11:47:30 -0700

    Added UserItem component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
-import Navbar from './components/layout/Navbar'
+
 
 class App extends Component {
   render() {
-    const name = 'Leah';
-    const loading = true;
-    const showName = false;
 
     return (
       <div className='App'>
-        <h1>My App</h1>
-        {loading ? <h4>Loading...</h4>: <h1>Hello {showName && name}</h1>}
+        <Navbar />
+        <UserItem />
       </div>
+      
     );
   }
 }

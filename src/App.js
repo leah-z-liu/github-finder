c93735955879b6eadc05b13a41437370c9bc97ac
oa8oht <leahliu779@gmail.com>
2019-06-22 11:05:59 -0700

    Added navbar component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar'
 
 class App extends Component {
   render() {
     const name = 'Leah';
     const loading = true;
+    const showName = false;
 
     return (
       <div className='App'>
         <h1>My App</h1>
-        {loading ? <h4>Loading...</h4>: <h1>Hello {name}</h1>}
+        {loading ? <h4>Loading...</h4>: <h1>Hello {showName && name}</h1>}
       </div>
     );
   }

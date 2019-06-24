94a24b4f66e7c18a94a0e8372945701aec93b8ab
oa8oht <leahliu779@gmail.com>
2019-06-23 18:06:10 -0700

    Refactored user item component

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 
-export class Navbar extends Component {
-    static defaultProps = {
-        icon: 'fab fa-github',
-        title: 'Github Finder'
-    };
-
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        icon: PropTypes.string.isRequired
-    };
-
-    render() {
-        return (
-            <nav className='navbar bg-primary'>
-                <h1>
-                    <i className={this.props.icon} /> 
-                    {this.props.title}
-                </h1>
-            </nav>
-        )
-    }
+const Navbar = ({ icon, title }) => {
+    return (
+        <nav className='navbar bg-primary'>
+            <h1>
+                <i className={icon} /> 
+                {title}
+            </h1>
+        </nav>
+    ) 
 }
+
+Navbar.defaultProps = {
+    icon: 'fab fa-github',
+    title: 'Github Finder'
+};
+
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+};
 
 export default Navbar
